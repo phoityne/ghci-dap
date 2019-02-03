2ff3b293db04181cd3e621bffde52ff95df1751e
phoityne.hs <phoityne.hs@gmail.com>
2019-02-03 11:29:36 +0900

    initial commit.

---
+++ app/Main.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main where
+
+import qualified GHCMain as G
+import qualified GHCi.UI as G
+import Control.Concurrent
+import Haskell.DAP.GHCi.Command
+import Haskell.DAP.GHCi.Type
+
+import Paths_ghci_dap (version)
+import Data.Version
+
+-- |
+--  Main
+--
+main :: IO ()
+main = do
+  putStrLn $ "[DAP][INFO] start ghci-dap-" ++ showVersion version ++ "."
+
+  mvarCtx <- newMVar defaultDAPContext
+
+  let ghciSettings    = G.defaultGhciSettings mvarCtx
+      defaultCommands = G.availableCommands ghciSettings
+      withDapCommands = defaultCommands ++ (dapCommands mvarCtx)
+
+  G.ghcMain ghciSettings {G.availableCommands = withDapCommands}
+  
