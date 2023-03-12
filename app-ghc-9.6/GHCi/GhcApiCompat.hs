4311f3e1b0ccdbf61bc6fcaf3a98a46fb8d57bee
phoityne <phoityne.hs@gmail.com>
2023-03-13 07:34:01 +0900

    support ghc-9.6.1.

---
+++ app-ghc-9.6/GHCi/GhcApiCompat.hs
@@ -0,0 +1,63 @@
+module GHCi.GhcApiCompat (
+    module GHC.Runtime.Heap.Inspect
+  , module GHC.Unit
+  , module GHC.Driver.Monad
+  , module GHC.Unit.Module
+  , module GHC.Utils.Outputable
+  , module GHC.Utils.Exception
+  , module GHC.Runtime.Debugger
+  , module GHC.Data.FastString
+  , module GHC.Runtime.Eval.Types
+  , module GHC.Core.DataCon
+  , module GHC.Types.TyThing.Ppr
+  , module GHC.Driver.Session
+  , module GHC.Types.Name.Reader
+  , module GHC.Types.SourceError
+  , module GHC.Driver.Ppr
+  , module GHC.Types.TyThing
+  , module GHC.Unit.Module.ModSummary
+  , module GHC.Driver.Env.Types
+  , module GHC.Runtime.Context
+  , module GHC.Types.BreakInfo
+  , module GHC.Types.FieldLabel
+  , CMC.catch
+  , pprTypeForUser
+  , ic_rn_gbl_env
+) where
+
+import GHC.Runtime.Heap.Inspect
+import GHC.Unit
+import GHC.Driver.Monad
+import GHC.Unit.Module
+import GHC.Utils.Outputable
+import GHC.Utils.Exception hiding (catch)
+import GHC.Runtime.Debugger
+import GHC.Data.FastString
+import GHC.Runtime.Eval.Types
+import GHC.Core.DataCon
+import GHC.Types.TyThing.Ppr
+import GHC.Driver.Session
+import GHC.Types.Name.Reader
+import GHC.Types.SourceError
+import GHC.Driver.Ppr
+import GHC.Types.TyThing
+import GHC.Unit.Module.ModSummary
+import GHC.Driver.Env.Types
+import GHC.Runtime.Context
+import GHC.Types.BreakInfo
+import GHC.Core.TyCo.Ppr
+import GHC.Core.Type
+import GHC.Types.FieldLabel
+import qualified Control.Monad.Catch as CMC
+
+-- |
+--
+--
+pprTypeForUser :: Type -> SDoc
+pprTypeForUser = pprSigmaType
+
+-- |
+--
+--
+ic_rn_gbl_env :: InteractiveContext -> GlobalRdrEnv
+ic_rn_gbl_env = igre_env . ic_gre_cache
